f1977b73dcf9dca8ef8a8fbb2ae262fc4d30105a
Yurij Mikhalevich <0@39.yt>
2017-02-21 23:11:48 +0300

    cleanup

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -124,11 +124,11 @@ void MainWindow::on_actionExportCsv_triggered() {
   QTextStream out(&file);
   for (PhaseGraphWidget* widget : phaseGraphWidgets_) {
     PhaseGraph* graph = widget->graph();
-    out << "\"" << "maxX, maxY" << "\"," << graph->maxX() << "," << graph->maxY() << "\n";
-    out << "\"" << "minX, minY" << "\"," << graph->minX() << "," << graph->minY() << "\n";
-    out << "\"" << "max, min" << "\"," << graph->max() << "," << graph->min() << "\n";
-    out << "square" << "," << graph->square() << "\n";
+    out << "\"maxX, maxY\"," << graph->maxX() << "," << graph->maxY() << "\n";
+    out << "\"minX, minY\"," << graph->minX() << "," << graph->minY() << "\n";
+    out << "\"max, min\"," << graph->max() << "," << graph->min() << "\n";
+    out << "square," << graph->square() << "\n";
     const QPointF& center = graph->center();
-    out << "center" << "," << center.x() << "," << center.y() << "\n\n";
+    out << "center," << center.x() << "," << center.y() << "\n\n";
   }
 }
